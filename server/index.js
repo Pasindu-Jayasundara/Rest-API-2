355fa75697c78534b6d3f024e1db364476417c68
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-03-07 15:13:48 +0530

    add user registration endpoint and update MongoDB connection string

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const mongoose = require('mongoose');
+const userModel = require('./models/User');
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(cors());
 app.listen(3003, async (req,res)=>{
 
     try {
-        await mongoose.connect('mongodb+srv://pasindujayasundara28:4OoSVOxppCZZog7X@cluster0.k1qyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://pasindujayasundara28:4OoSVOxppCZZog7X@cluster0.k1qyn.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Server is running on port 3003');
     } catch (error) {
         console.log(error);
@@ -23,9 +24,18 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
-app.post('/registration',(req,res)=>{
+app.post('/registration',async (req,res)=>{
     
-    const body = req.body;
-    console.log(body);
+    const {username,password} = req.body;
+
+    // save
+    const createdUser = await userModel.create(
+        {
+            username,
+            password
+        }
+    );
+
+    res.send(createdUser);
 
 });
